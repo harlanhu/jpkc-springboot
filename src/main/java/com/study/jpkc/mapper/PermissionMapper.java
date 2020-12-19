04387935776c61def25a0086df0ba4435ff06c21
keyboardWithDream <isharlan.hu@gmail.com>
2020-12-19 20:28:50 +0800

    mapper更新

---
+++ src/main/java/com/study/jpkc/mapper/PermissionMapper.java
@@ -0,0 +1,16 @@
+package com.study.jpkc.mapper;
+
+import com.study.jpkc.entity.Permission;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author isharlan.hu@gmail.com
+ * @since 2020-12-19
+ */
+public interface PermissionMapper extends BaseMapper<Permission> {
+
+}
---
+++ src/main/java/com/study/jpkc/mapper/RoleMapper.java
@@ -0,0 +1,16 @@
+package com.study.jpkc.mapper;
+
+import com.study.jpkc.entity.Role;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author isharlan.hu@gmail.com
+ * @since 2020-12-19
+ */
+public interface RoleMapper extends BaseMapper<Role> {
+
+}
