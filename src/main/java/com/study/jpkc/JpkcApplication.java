e38ed9d414f0dfb26f9745275f57eaf820cf59ff
keyboardWithDream <isharlan.hu@gmail.com>
2020-12-19 14:56:32 +0800

    添加权限与角色

--- src/main/java/com/study/jpkc/JpkcApplication.java
+++ src/main/java/com/study/jpkc/JpkcApplication.java
@@ -1,6 +1,5 @@
 package com.study.jpkc;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Harlan
  */
 @SpringBootApplication
-@MapperScan("com.study.jpkc.mapper")
 public class JpkcApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/study/jpkc/service/impl/AccountServiceImpl.java
+++ src/main/java/com/study/jpkc/service/impl/AccountServiceImpl.java
@@ -1,6 +1,8 @@
 package com.study.jpkc.service.impl;
 
 import com.study.jpkc.service.IAccountService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
  * @Date 2020/12/18
  */
 @Service
+@Slf4j
 @Transactional(rollbackFor = Exception.class)
 public class AccountServiceImpl implements IAccountService {
+
 }
