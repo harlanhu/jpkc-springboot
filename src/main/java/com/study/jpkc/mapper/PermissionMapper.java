947d9f6c04a53513c86a2e31710bdbbd7bde2b8a
keyboardWithDream <isharlan.hu@gmail.com>
2020-12-29 19:54:51 +0800

    插件配置mapper.xml

--- src/main/java/com/study/jpkc/mapper/PermissionMapper.java
+++ src/main/java/com/study/jpkc/mapper/PermissionMapper.java
@@ -3,13 +3,14 @@ package com.study.jpkc.mapper;
 import com.study.jpkc.entity.Permission;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author isharlan.hu@gmail.com
@@ -24,5 +25,5 @@ public interface PermissionMapper extends BaseMapper<Permission> {
      * @return 权限信息
      */
     @Select("select * from t_permission where permission_id in (select permission_id from tm_role_permission where role_id = #{roleId})")
-    List<Permission> findPermissionsByRoleId(String roleId);
+    List<Permission> findPermissionsByRoleId(@Param("roleId") String roleId);
 }
