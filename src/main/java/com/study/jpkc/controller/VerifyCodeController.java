ef2f6b922dde56eb8155731c3e0b4e03dcde8f0e
keyboardWithDream <isharlan.hu@gmail.com>
2021-01-03 20:54:24 +0800

    fix(VerifyCodeController): 解决验证码重复问题

--- src/main/java/com/study/jpkc/controller/VerifyCodeController.java
+++ src/main/java/com/study/jpkc/controller/VerifyCodeController.java
@@ -1,8 +1,11 @@
 package com.study.jpkc.controller;
 
 import com.study.jpkc.common.component.KaptchaComponent;
+import com.study.jpkc.common.lang.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author Harlan
@@ -15,13 +18,30 @@ public class VerifyCodeController {
     @Autowired
     private KaptchaComponent kaptchaComponent;
 
+    /**
+     * 获取验证码
+     */
     @GetMapping("/getCode")
     public void render() {
         kaptchaComponent.render();
     }
 
+    /**
+     * 校验验证码并删除
+     * @param code 验证码
+     */
     @PostMapping("/validCode")
     public void validCustomTime(@RequestParam String code) {
         kaptchaComponent.validate(code);
+    }
+
+    /**
+     * 校验验证码
+     * @param coed 验证码
+     * @return 返回结果
+     */
+    @PostMapping("/isValidCode")
+    public Result isValidCode(@RequestParam String coed) {
+        return Result.getSuccessRes(kaptchaComponent.isValidate(coed));
     }
 }
