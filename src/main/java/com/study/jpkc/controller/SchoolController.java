7b9c919d66ca79e9cbea4260d5043a6f71499150
keyboardWithDream <isharlan.hu@gmail.com>
2021-01-17 23:15:32 +0800

    feat(SchoolController): 根据学校id查询学校信息

--- src/main/java/com/study/jpkc/controller/SchoolController.java
+++ src/main/java/com/study/jpkc/controller/SchoolController.java
@@ -1,6 +1,12 @@
 package com.study.jpkc.controller;
 
 
+import com.study.jpkc.common.lang.Result;
+import com.study.jpkc.service.ISchoolService;
+import org.apache.shiro.authz.annotation.RequiresGuest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/school")
 public class SchoolController {
+
+    @Autowired
+    private ISchoolService schoolService;
+
+    @RequiresGuest
+    @GetMapping("/getSchoolById/{schoolId}")
+    public Result getSchoolById(@PathVariable String schoolId) {
+        return Result.getSuccessRes(schoolService.getById(schoolId));
+    }
 
 }
