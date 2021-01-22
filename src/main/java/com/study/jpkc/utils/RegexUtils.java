3ad298b6b808889926e47ce93ceca7ccc70711ff
keyboardWithDream <isharlan.hu@gmail.com>
2021-01-22 08:33:35 +0800

    update

--- src/main/java/com/study/jpkc/utils/RegexUtils.java
+++ src/main/java/com/study/jpkc/utils/RegexUtils.java
@@ -10,7 +10,7 @@ public class RegexUtils {
     public static final String EMAIL_REGEX = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
     public static final String PHONE_REGEX = "^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\\d{8}$";
     public static final String USERNAME_REGEX = "^[a-zA-Z][a-zA-Z0-9_]{3,15}$";
-    public static final String PASSWORD_REGEX = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,10}$";
+    public static final String PASSWORD_REGEX = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$";
     public static final String VERIFY_CODE_REGEX = "^[A-Za-z0-9]{4}$";
     public static final String SMS_VERIFY_CODE_REGEX = "^[0-9]{4}$";
 
