289d62ae4c82aa13c851a9d31685e6ad0e14a048
keyboardWithDream <isharlan.hu@gmail.com>
2021-01-27 15:50:40 +0800

    feat(smsverify): 更改为restful风格

--- src/main/java/com/study/jpkc/controller/SmsController.java
+++ src/main/java/com/study/jpkc/controller/SmsController.java
@@ -4,10 +4,7 @@ import com.study.jpkc.common.component.SmsComponent;
 import com.study.jpkc.common.lang.Result;
 import com.study.jpkc.utils.RegexUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author Harlan
@@ -26,8 +23,8 @@ public class SmsController {
      * @param phone 手机号
      * @return 返回信息
      */
-    @PostMapping("getSmsVerifyCode")
-    public Result getSmsVerifyCode(@RequestBody String phone) {
+    @GetMapping("getSmsVerifyCode/{phone}")
+    public Result getSmsVerifyCode(@PathVariable String phone) {
         if (!RegexUtils.phoneMatches(phone)) {
             return Result.getFailRes(RegexUtils.INCORRECT_FORMAT_PHONE);
         }
