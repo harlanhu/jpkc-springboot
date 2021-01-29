8c3dbf0d69ef565e875a6e68af7fac22a6bbc62d
keyboardWithDream <isharlan.hu@gmail.com>
2021-01-29 17:34:38 +0800

    feat(PageVo): 分页查询

---
+++ src/main/java/com/study/jpkc/common/lang/PageVo.java
@@ -0,0 +1,37 @@
+package com.study.jpkc.common.lang;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @Author Harlan
+ * @Date 2021/1/29
+ */
+@Data
+public class PageVo implements Serializable {
+
+    private List<?> list;
+    private Long current;
+    private Long size;
+    private Long total;
+    private Long pages;
+
+    public PageVo(IPage<?> pageData) {
+        this.list = pageData.getRecords();
+        this.current = pageData.getCurrent();
+        this.size = pageData.getSize();
+        this.pages = pageData.getPages();
+        this.total = pageData.getTotal();
+    }
+
+    public static PageVo getPageVo(IPage<?> pageData) {
+        return new PageVo(pageData);
+    }
+
+    public static Result getResult(IPage<?> pageData) {
+        return Result.getSuccessRes(getPageVo(pageData));
+    }
+}
