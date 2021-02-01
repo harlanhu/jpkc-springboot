1f0884fe0db5eed2d1b05b6a92a360aea8b08962
keyboardWithDream <isharlan.hu@gmail.com>
2021-02-01 18:01:00 +0800

    feat(Application): 开启定时任务

--- src/main/java/com/study/jpkc/JpkcApplication.java
+++ src/main/java/com/study/jpkc/JpkcApplication.java
@@ -3,10 +3,12 @@ package com.study.jpkc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author Harlan
  */
+@EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties
 public class JpkcApplication {
