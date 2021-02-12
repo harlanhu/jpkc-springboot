2cc7ac5c5a69d3af39006d040f52c2b9eb5b970e
keyboardWithDream <isharlan.hu@gmail.com>
2021-02-12 19:28:36 +0800

    feat(WebSocket): WebSocket配置信息

---
+++ src/main/java/com/study/jpkc/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.study.jpkc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * @Author Harlan
+ * @Date 2021/2/12
+ * @Desc WebSocket长连接通信
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/jpck-websocket")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
