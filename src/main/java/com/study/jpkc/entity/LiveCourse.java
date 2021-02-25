08f18282db2ca5735da5a5b5fddd462d05bb5f58
keyboardWithDream <isharlan.hu@gmail.com>
2021-02-25 17:59:36 +0800

    feat(entity): update

--- src/main/java/com/study/jpkc/entity/LiveCourse.java
+++ src/main/java/com/study/jpkc/entity/LiveCourse.java
@@ -1,5 +1,6 @@
 package com.study.jpkc.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,6 +23,7 @@ public class LiveCourse implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @TableId
     private String liveCourseId;
 
     private String teacherId;
