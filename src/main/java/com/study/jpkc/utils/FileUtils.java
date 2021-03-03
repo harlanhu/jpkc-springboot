29f88c3d4bc770eef47f5dfd31890b8b32024f0d
keyboardWithDream <isharlan.hu@gmail.com>
2021-03-03 18:00:39 +0800

    feat(file): 文件工具类

---
+++ src/main/java/com/study/jpkc/utils/FileUtils.java
@@ -0,0 +1,22 @@
+package com.study.jpkc.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @Author Harlan
+ * @Date 2021/3/3
+ */
+public class FileUtils {
+
+    private static final String[] PICTURE_TYPE = {"image/jpeg", "image/jpg", "image/png"};
+
+    public static Boolean isTypeOfPicture(MultipartFile file) {
+        String fileType = file.getContentType();
+        for (String s : PICTURE_TYPE) {
+            if (s.equals(fileType)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
