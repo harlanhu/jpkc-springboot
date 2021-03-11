e5c6ebbb94c978219dceb204855e804dd0dcf007
keyboardWithDream <isharlan.hu@gmail.com>
2021-03-11 11:47:11 +0800

    update

--- src/test/java/com/study/jpkc/utils/FileUtilsTest.java
+++ src/test/java/com/study/jpkc/utils/FileUtilsTest.java
@@ -15,7 +15,7 @@ class FileUtilsTest {
 
     @Test
     void getFileUrlPathTest() throws MalformedURLException {
-        URL url = new URL("http://web-applications.oss-cn-chengdu.aliyuncs.com/jpck/course/5a22571cd942475aae7fe24aa596b451/logo/courseLogo.png?Expires=1615271192&OSSAccessKeyId=LTAI4GHwUUA7g2o474KgGesL&Signature=56sQcQuWo44zmzKGeBePzCBhUEU%3D");
+        URL url = new URL("http://web-applications.oss-cn-chengdu.aliyuncs.com/jpck/course/5a22571cd942475aae7fe24aa596b451/logo/courseLogo.png?Expires=1615271192&OSSAccessKeyId=AdedSDDDDDDWdqwd&Signature=56sQcQuWo44zmzKGeBePzCBhUEU%3D");
         String urlPath = FileUtils.getFileUrlPath(url);
         assertThat(urlPath).isNotBlank();
         System.out.println(urlPath);
