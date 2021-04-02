01f6bde649d8c129eb9bfee84651ab80e6a626d8
keyboardWithDream <isharlan.hu@gmail.com>
2021-04-02 10:43:43 +0800

    update

--- src/main/java/com/study/jpkc/config/WebSocketConfig.java
+++ src/main/java/com/study/jpkc/config/WebSocketConfig.java
@@ -1,13 +1,8 @@
 package com.study.jpkc.config;
 
-import com.study.jpkc.interceptor.ChannelInterceptor;
-import com.study.jpkc.interceptor.HandShakeInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * @Author Harlan
@@ -15,37 +10,11 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
  * @Desc WebSocket长连接通信
  */
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig {
 
     @Bean
-    public ChannelInterceptor channelInterceptor() {
-        return new ChannelInterceptor();
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        //添加访问域名限制可以防止跨域socket连接
-        //setAllowedOrigins("http://localhost:8080")
-        registry.addEndpoint("/live")
-                .setAllowedOrigins("*")
-                .addInterceptors(new HandShakeInterceptor())
-                .withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        /*.enableSimpleBroker("/topic","/queue");*/
-        //假如需要第三方消息代理，比如RabbitMQ在这里配置
-        registry.setApplicationDestinationPrefixes("/live")
-                .enableStompBrokerRelay("/topic", "/queue")
-                .setRelayHost("47.108.151.199")
-                .setRelayPort(5672)
-                .setClientLogin("mqAdmin")
-                .setClientPasscode("lb82ndLF-mq")
-                .setSystemLogin("mqAdmin")
-                .setSystemPasscode("lb82ndLF-mq")
-                .setSystemHeartbeatReceiveInterval(5000)
-                .setSystemHeartbeatSendInterval(5000);
-    }
 }
