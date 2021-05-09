b4d3a130f6ab03af66caf16f9c8b236b629c57a0
keyboardWithDream <isharlan.hu@gmail.com>
2021-05-09 12:52:08 +0800

    update

--- src/main/java/com/study/jpkc/controller/StudentController.java
+++ src/main/java/com/study/jpkc/controller/StudentController.java
@@ -1,17 +1,18 @@
 package com.study.jpkc.controller;
 
 
+import cn.hutool.core.util.IdUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.study.jpkc.common.lang.PageVo;
 import com.study.jpkc.common.lang.Result;
 import com.study.jpkc.entity.Student;
+import com.study.jpkc.entity.User;
 import com.study.jpkc.service.IStudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.study.jpkc.service.IUserService;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -27,8 +28,11 @@ public class StudentController {
 
     private final IStudentService studentService;
 
-    public StudentController(IStudentService studentService) {
+    private final IUserService userService;
+
+    public StudentController(IStudentService studentService, IUserService userService) {
         this.studentService = studentService;
+        this.userService = userService;
     }
 
     @GetMapping("/getAll/{current}/{size}")
@@ -40,5 +44,21 @@ public class StudentController {
     public Result getByName(@PathVariable String studentName, @PathVariable int current, @PathVariable int size) {
         return Result.getSuccessRes(PageVo.getPageVo(studentService.page(new Page<>(current, size),
                 new QueryWrapper<Student>().eq("student_name", studentName))));
+    }
+
+    @PostMapping("/save")
+    public Result save(@RequestBody Student student) {
+        User user = new User();
+        String userId = IdUtil.simpleUUID();
+        user.setUserId(userId);
+        user.setPassword("Aa123456");
+        user.setUserSex(0);
+        user.setUserCreated(LocalDateTime.now());
+        user.setUserStatus(0);
+        student.setStudentId(IdUtil.simpleUUID());
+        student.setUserId(userId);
+        userService.save(user);
+        studentService.save(student);
+        return Result.getSuccessRes(null);
     }
 }
